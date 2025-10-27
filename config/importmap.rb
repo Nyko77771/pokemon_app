69ef9ad89a0c2c945c602c40c8e2ddef75e09b12
Nyko77771 <nykytamcdonald@gmail.com>
2025-10-27 14:09:01 +0000

    Bootstrap Gem

--- config/importmap.rb
+++ config/importmap.rb
@@ -4,4 +4,6 @@ pin "application"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
+pin "bootstrap", to: "bootstrap.js", preload: true
+pin "@popperjs/core", to: "popper.min.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"

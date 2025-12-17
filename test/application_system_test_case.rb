ec43b6febef3c4c00567b4d76d5098f15f30c4e1
Nyko77771 <nykytamcdonald@gmail.com>
2025-12-17 15:25:36 +0000

    Gem

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 require 'test_helper'
-require 'selenium-webdriver'
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   driven_by :selenium, using: :firefox, screen_size: [1400, 1400]
